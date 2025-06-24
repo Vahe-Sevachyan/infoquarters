bb9dd83b567f85c602f4c40d983a12e1d39da006
Vahe Sevachyan <vscode13@gmail.com>
2025-06-23 20:47:08 -0700

    added PostForm component

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,6 +1,31 @@
 import { useState } from "react";
 
 function PostForm({ initialData = {}, onSubmit }) {
-  <></>;
+  const [title, setTitle] = useState(initialData.title || "");
+  const [content, setContent] = useState(initialData.content || "");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ title, content });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <textarea
+        placeholder="Content"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        required
+      />
+      <button type="submit">Save</button>
+    </form>
+  );
 }
+
 export default PostForm;
